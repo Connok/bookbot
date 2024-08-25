66f19bfb5d6c93b6c5065f3948a6288284946093
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-08-24 21:49:56 -0400

    count number of words in book

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+        book_path = "books/frankenstein.txt"
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        print(f"{num_words} words found in the document")
+
+def get_num_words(text):
+     words = text.split()
+     return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
